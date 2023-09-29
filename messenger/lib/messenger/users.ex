d896f74081b65d304c73bd5255674ed3fafb77a8
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2023-09-29 23:51:47 +0300

    feat: password + password hash with bcrypt

--- messenger/lib/messenger/users.ex
+++ messenger/lib/messenger/users.ex
@@ -7,6 +7,8 @@ defmodule Messenger.User do
 
   schema "users" do
     field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
     has_many(:messages, Messenger.Message, foreign_key: :sender_id)
 
     timestamps()
@@ -23,7 +25,21 @@ defmodule Messenger.User do
   end
 
   def create_user_changeset(user, attrs) do
-    user |> cast(attrs, [:email]) |> validate_required([:email]) |> validate_email(:email)
+    user
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_email(:email)
+    |> validate_length(:password, min: 2)
+    |> put_password_hash(:password)
+  end
+
+  def put_password_hash(changeset, field) do
+    if changeset.valid? do
+      password_hash = Bcrypt.Base.hash_password("hard to guess", Bcrypt.Base.gen_salt(12, true))
+      changeset |> put_change(:password_hash, password_hash)
+    else
+      changeset
+    end
   end
 
   def create_user(user) do
@@ -33,5 +49,9 @@ defmodule Messenger.User do
 
   def get_user_by_id(user_id) do
     Repo.get_by!(User, id: user_id)
+  end
+
+  def get_messages_by_user_id(user_id) do
+    get_user_by_id(user_id) |> Repo.preload(:messages) |> Map.get(:messages)
   end
 end
---
+++ messenger/priv/repo/migrations/20230929202221_add_password_hash_to_users_table.exs
@@ -0,0 +1,9 @@
+defmodule Messenger.Repo.Migrations.AddPasswordHashToUsersTable do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :password_hash, :string
+    end
+  end
+end
