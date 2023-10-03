d74d2962b148eaa29a50d215bababbcd31f18ff2
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2023-10-03 23:06:44 +0300

    feat: Session hook

--- messenger/assets/js/app.js
+++ messenger/assets/js/app.js
@@ -16,26 +16,70 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+
+const LocalStateStore = {
+  mounted() {
+    this.handleEvent("setSessionStorageItem", (obj) =>
+      this.setSessionStorageItem(obj)
+    ),
+      this.handleEvent("restore", (obj) => this.restore(obj));
+  },
+  setSessionStorageItem({ name, item }) {
+    console.log("event");
+    sessionStorage.setItem(name, item);
+  },
+  restore({ name, event }) {
+    item = sessionStorage.getItem(name);
+    console.log(event);
+    this.pushEvent(event, item);
+  },
+};
+
+let Hooks = {};
+
+Hooks.LocalStateStore = LocalStateStore;
+Hooks.Session = {
+  mounted() {
+    this.handleEvent("setSession", ({ token }) => {
+      sessionStorage.setItem("token", token);
+
+      fetch(`/auth`, {
+        method: "post",
+        body: JSON.stringify({ token }),
+        credentials: "include",
+        headers: {
+          "x-csrf-token": csrfToken,
+          "Content-Type": "application/json",
+        },
+      });
+    });
+  },
+};
+
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
