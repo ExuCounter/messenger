01f0cc5b9efcd6d1ed9983228e8eef0b02333f2a
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2023-10-16 19:56:36 +0300

    feat: add seeds

--- messenger/priv/repo/seeds.exs
+++ messenger/priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule Messenger.Seeds do
+  alias Messenger.{User, Chat, Message, Repo}
+
+  def seeds do
+    clear()
+
+    {:ok, user} = User.create_user(%{email: "test@gmail.com", password: "test"})
+    {:ok, chat} = Chat.create_chat(%{user_id: user.id, title: "Test chat"})
+    Message.send_message(%{user_id: user.id, chat_id: chat.id, body: "message"})
+  end
+
+  def clear do
+    Repo.delete_all(Message)
+    Repo.delete_all(Chat)
+    Repo.delete_all(User)
+  end
+end
+
+Messenger.Seeds.seeds()
